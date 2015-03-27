fix gradient error

mnist/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy as sp
 import sgd
 import mlp
 import mnist
@@ -15,6 +16,17 @@ def showimage(m, maxval):
     imshow(m, cmap=cm.gray)
     show()
 
+def downsample(images):
+    # assumption: dimensions are (n,h,w)
+    (n,h,w) = images.shape
+    assert h % 2 == 0
+    assert w % 2 == 0
+    result = np.zeros((n,h/2,w/2))
+    for hh in xrange(0,h/2,1):
+        for ww in xrange(0,w/2,1):
+            result[:,hh,ww] = images[:,hh*2:hh*2+1,ww*2:ww*2+1].mean(axis=(1,2))
+    return result
+
 def do_mnist():
     np.random.seed(1003)
     images, labels = mnist.load_mnist('training')
@@ -22,17 +34,22 @@ def do_mnist():
     images = images.astype(float)
     images = (images - images.mean())
 
-    maxd = 784
+    #images = downsample(images)
+    #images = downsample(images)
 
-    n = images.shape[0]
-    mbsize = 10
-    niter = 10000
+    maxd = images.shape[1] * images.shape[2]
 
-    lr = .001
-    (lrstep, lrmult) = (100, .9)
-    l2reg = 0.0001
+    (n,H,W) = images.shape
+
+    n = 10000
+    mbsize = 100
+    niter = 5000
+
+    lr = .1
+    (lrstep, lrmult) = (1000, .9)
+    l2reg = 0.0
     max_grad_norm = .1 / lr
-    std = .1
+    std = .01
 
     X = images[:n,:,:]
     X = X.reshape((X.shape[0],-1)).T
@@ -47,50 +64,114 @@ def do_mnist():
 
     print "X shape = ", X.shape
 
-    #archstr = "a.{}.784_a.784.{}".format(d, d)
     #archstr = "a.{}.100_t_a.100.{}".format(d,d)
-    archstr = "a.{}.{}".format(d,d)
+    #archstr = "a.{}.100_s.100_a.100.100_s.100_a.100.{}".format(d,d)
+    #archstr = "a.{}.100_a.100.{}".format(d,d)
+    archstr = "a.{0}.{1}_t.{1}_a.{1}.{1}_t.{1}_a.{1}.{1}_t.{1}_a.{1}.{0}".format(d,50)
     affinit = mlp.GaussianAffineInitializer(std,std)
-    mlpa = mlp.MLPAutoencoder(archstr, l2reg, affinit)
+    mlpa = mlp.MLPAutoencoder(archstr, mbsize, l2reg, affinit)
 
     w = mlpa.w_debug()[0][0]
     #print np.linalg.norm(w), np.max(w), np.min(w), np.shape(w)
     #print "Done."
     #showimage(w, 1)
 
-    sgdobj = sgd.SGD(X, X, mbsize, lrstep, lrmult, lr, max_grad_norm=max_grad_norm)
-    sgdobj.train(mlpa, niter)
+    #mlpa.set_w(np.concatenate((np.eye(784).flatten(),np.zeros(784))))
 
-    #mlpa.fwd(X,X)
-    #mlpa.bwd()
-    #f = lambda: sum(mlpa.fwd(X,X)[1:])
-    #g = lambda: mlpa.grad()
-    #gradcheck.gradcheck(f, g, mlpa.w())
+    train = True
+    check = False
 
+    if train:
+        sgdobj = sgd.SGD(X, X, mbsize, lrstep, lrmult, lr, max_grad_norm=max_grad_norm)
+        sgdobj.train(mlpa, niter)
 
-    #w = mlpa.w_debug()[0][0]
-    #showimage(w, w.max())
-
-    (ypred, _, _) = mlpa.fwd(X,X)
     
-    s = 5
-    indices = np.random.choice(n, s)
-    
-    ypredsample = ypred[:,indices]
-    Xsample = X[:,indices]
-    
-    ypredsample = ypredsample.T.reshape((s, 28, 28))
-    Xsample = Xsample.T.reshape((s,28,28))
-    
-    a = np.lib.pad(ypredsample, ((0,0),(4,4),(4,4)), 'constant')
-    b = np.lib.pad(Xsample, ((0,0),(4,4),(4,4)), 'constant')
-    
-    a = a.reshape(-1,36)
-    b = b.reshape(-1,36)
+    if check:
+        mlpa.set_X_y(X[:,:mbsize], X[:,:mbsize])
+        
+        # Fwd and Bwd passes
+        mlpa.fwd(do_Hv=False, do_Gv=False)
+        mlpa.bwd(do_Hv=False, do_Gv=False)
+        f = lambda: sum(mlpa.fwd(do_Hv=False, do_Gv=False))
+        g = lambda: mlpa.get_g()
+        gradcheck.gradcheck(f, g, mlpa.get_w())
 
-    c = np.hstack((a,b))
 
-    showimage(c, c.max())
+    if train:
+        mlpa.set_X_y(X[:,:mbsize], X[:,:mbsize])
+        
+        (_, _) = mlpa.fwd(do_Hv=False, do_Gv=False)
+        
+        ypred = mlpa.get_ypred()
+        
+        s = 5
+        indices = np.random.choice(mbsize, s)
+        
+        ypredsample = ypred[:,indices]
+        Xsample = X[:,indices]
+        
+        ypredsample = ypredsample.T.reshape((s, H, W))
+        Xsample = Xsample.T.reshape((s,H,W))
+        
+        a = np.lib.pad(ypredsample, ((0,0),(4,4),(4,4)), 'constant')
+        b = np.lib.pad(Xsample, ((0,0),(4,4),(4,4)), 'constant')
+        
+        a = a.reshape(-1,W+8)
+        b = b.reshape(-1,W+8)
+        
+        c = np.hstack((a,b))
+        
+        showimage(c, c.max())
+        
+        w_debug = mlpa.w_debug()
+        
+        matrix0 = w_debug[0][0]
+        showimage(matrix0, matrix0.max())
+        
+
+def do_gradcheck():
+    np.random.seed(1003)
+
+    d = 1
+    n = 1
+    #X = np.random.randn(d,n)
+    X = np.ones((d,n))
+    std=.01
+    archstr = "a.{0}.{0}_a.{0}.{0}".format(d)
+    #archstr = "a.{0}.{0}".format(d)
+    affinit = mlp.GaussianAffineInitializer(std,std)
+    mlpa = mlp.MLPAutoencoder(archstr, n, 0.0, affinit)
+
+    #sgdobj = sgd.SGD(X, X, n, 1000, 1, .01)
+    #sgdobj.train(mlpa, 1000)
+
+    mlpa.set_X_y(X[...], X[...])
+    w = mlpa.get_w()
+    w[...] = 1.0
+
+    mlpa.fwd(do_Hv=False, do_Gv=False)
+    mlpa.bwd(do_Hv=False, do_Gv=False)
+
+    np.set_printoptions(threshold=np.inf)
+    # print "----------------------------------------"
+    # print "IO"
+    # print mlpa.iostack.m
+    # print "Params"
+    # print mlpa.paramstack.m
+
+    # mlpa.paramstack.val[0] = 0
+    # mlpa.fwd(do_Hv=False, do_Gv=False)
+    # print "----------------------------------------"
+    # print "IO"
+    # print mlpa.iostack.m
+    # print "Params"
+    # print mlpa.paramstack.m
+
+
+
+    f = lambda: sum(mlpa.fwd(do_Hv=False, do_Gv=False))
+    g = lambda: mlpa.get_g()
+    gradcheck.gradcheck(f, g, mlpa.get_w(), mlpa, delta=1)
     
     
     
@@ -106,6 +187,7 @@ def do_test():
     l2reg = .0001
     max_grad_norm = 1000000000000 / lr
     std = 0.0001
+    mbsize = N
 
     #X = (np.arange(N)+1).reshape(1,N)
     #X = np.concatenate((X,X**2,X**3))
@@ -122,14 +204,14 @@ def do_test():
     #archstr = "a.{}.784_a.784.{}".format(d, d)
     archstr = "a.{}.{}".format(d,d)
     affinit = mlp.GaussianAffineInitializer(std,std)
-    mlpa = mlp.MLPAutoencoder(archstr, l2reg, affinit)
+    mlpa = mlp.MLPAutoencoder(archstr, mbsize, l2reg, affinit)
 
     #mlpa.fwd(X,y)
     #mlpa.bwd()
     #f = lambda: sum(mlpa.fwd(X,y)[1:])
     #g = lambda: mlpa.grad()
     #gradcheck.gradcheck(f, g, mlpa.w())
-    sgdobj = sgd.SGD(X, y, n, lrstep, lrmult, lr, max_grad_norm=max_grad_norm)
+    sgdobj = sgd.SGD(X, y, mbsize, lrstep, lrmult, lr, max_grad_norm=max_grad_norm)
     sgdobj.train(mlpa, niter)
     
 
@@ -138,3 +220,4 @@ def do_test():
 if __name__ == "__main__":
     do_mnist()
     #do_test()
+    #do_gradcheck()

mnist/mlp.py
@@ -2,7 +2,6 @@ import numpy as np
 import layers
 import sys
 import re
-from enum import Enum
 from blobstack import BlobStack
 
 class AbstractAffineInitializer(object):
@@ -17,8 +16,8 @@ class GaussianAffineInitializer(AbstractAffineInitializer):
         self.bstd = bstd;
 
     def initialize(self, w, b):
-        w[...] = self.wstd * np.random.randn(w.size)
-        b[...] = self.bstd * np.random.randn(b.size)
+        w[...] = self.wstd * np.random.standard_normal(w.shape)
+        b[...] = self.bstd * np.random.standard_normal(b.shape)
 
         
 
@@ -38,7 +37,7 @@ class MLPAutoencoder(object):
         totalPSize = 0
         totalIOSize = 0
         for i,tok in enumerate(tokens):
-            (isize, osize, psize) = self.makeLayer(tok, l2reg, mbsize, None, None, None, getParamSize=True)
+            (isize, osize, psize) = self.makeLayer(tok, l2reg, mbsize, None, None, None)
             self.layerSizes.append((isize, osize, psize))
             totalPSize += psize
             if i==0: totalIOSize += isize
@@ -57,6 +56,7 @@ class MLPAutoencoder(object):
         iostart=0
         for i, (tok, (isize, osize, psize)) in enumerate(zip(tokens,self.layerSizes)):
             pslice = self.paramstack.subblob(pstart,pstart+psize)
+            pstart += psize
 
             islice = self.iostack.subblob(iostart, iostart+isize)
             iostart += isize
@@ -68,7 +68,7 @@ class MLPAutoencoder(object):
             if i==0:
                 istack = layer.get_istack()
                 self.X = istack.val
-            if i==len(self.layerSizes-1):
+            if i==len(self.layerSizes)-1:
                 ostack = layer.get_ostack()
                 self.ypred = ostack.val
 
@@ -78,21 +78,42 @@ class MLPAutoencoder(object):
         # Likewise, this field represents the gradient as a flat vector
         self.g = self.paramstack.g
 
+        # This field represents the vector "v" in the product Hv, again as a flat vector
+        self.v = self.paramstack.gtval
+
+        # For each minibatch we will assign to self.y, so for now it's None
+        self.y = None
+
         self.l2reg = l2reg
 
-    #TODO
     def set_v(self, v):
-        pass
+        assert v.shape == self.v.shape
+        self.v[...] = v
 
-    #TODO
-    def load_w(self, w):
-        pass
+    def get_g(self):
+        return self.g
 
-    #TODO
-    def load_X_y(self, X, y):
-        pass
+    def get_w(self):
+        return self.w
+
+    def set_w(self, w):
+        assert w.shape == self.w.shape
+        self.w[...] = w
+
+    def set_X_y(self, X, y):
+        assert X.shape == self.X.shape
+        assert y.shape == self.ypred.shape
+
+        # Note that self.y is one of the only cases where we assign a reference rather
+        # than populating in-place.
+        self.X[...] = X
+        self.y = y
+        self.ypred[...] = 0
+
+    def get_ypred(self):
+        return self.ypred
         
-    # TODO include Hv logic, eventaully generalize to softmax/crossentropy
+    # TODO eventaully generalize to softmax/crossentropy
     def prop_data_loss(self, do_Hv=False, do_Gv=False):
 
         go  = self.layers[-1].get_ostack().g
@@ -119,7 +140,6 @@ class MLPAutoencoder(object):
 
         return loss
 
-    #TODO: modify SGD to use make_blobs and use input/output memory cleanly
     def fwd(self, do_Hv=False, do_Gv=False):
         reg_loss = 0
         data_loss = None
@@ -152,7 +172,7 @@ class MLPAutoencoder(object):
         return wlist
 
     def makeLayer(self, specifier, l2reg, mbsize, pstack, istack, ostack):
-        print "Layer spec: ",specifier
+        #print "Layer spec: ",specifier
         maff = re.match(r"a\.(\d+)\.(\d+)$", specifier)
         mrelu = re.match(r"r\.(\d+)$", specifier)
         mtanh = re.match(r"t\.(\d+)$", specifier)
@@ -196,7 +216,7 @@ class MLPAutoencoder(object):
             istack.reshape_all((din,mbsize))
             ostack.reshape_all((dout,mbsize))
 
-            self.affinit.initialize(wstack.m, bstack.m)
+            self.affinit.initialize(wstack.val, bstack.val)
 
             return layers.AffineLayer(wstack, bstack, istack, ostack, l2reg)
             

mnist/sgd.py
@@ -21,7 +21,9 @@ class SGD(object):
         self.max_grad_norm = max_grad_norm
         
     def train(self, mlp, mbn):
-        
+
+        wdelta = np.zeros_like(mlp.get_w())
+
         for i in xrange(mbn):
             end = self.start + self.mb  
             ## because always divisible, this won't break
@@ -29,14 +31,16 @@ class SGD(object):
             X_curr = self.X[:,self.start:end]
             y_curr = self.y[:,self.start:end]
 
+            mlp.set_X_y(X_curr, y_curr)
+            
             # Fwd and Bwd passes
-            (_, data_loss, reg_loss) = mlp.fwd(X_curr, y_curr)
-            mlp.bwd()
+            (data_loss, reg_loss) = mlp.fwd(do_Hv=False, do_Gv=False)
+            mlp.bwd(do_Hv=False, do_Gv=False)
                         
-            grad = mlp.grad()
-            w = mlp.w()
+            grad = mlp.get_g()
+            w = mlp.get_w()
 
-            wdelta = -grad * self.lr
+            wdelta[...] = -grad * self.lr
 
             # Compute some stats for reporting
             norm_wdelta = np.linalg.norm(wdelta)
@@ -46,7 +50,7 @@ class SGD(object):
             
             #if self.curriter % 10 == 1: 
             #    print "data_loss={:.2E}  reg_loss={:.2E}  lr={:.2E}  w={}  -grad={}".format(data_loss, reg_loss, self.lr, w, -grad)
-            if self.curriter % 10 == 0: print "data_loss={:.2E}  reg_loss={:.2E}  lr={:.2E}  orig_grad={:.2E}  clip_grad={:.2E}  {}  stepsize={:.2E}  wnorm={:.2E}  wmin,max={:.2E},{:.2E}".format(data_loss, reg_loss, self.lr, np.linalg.norm(grad), np.linalg.norm(grad), ("C" if False else "."), norm_wdelta, norm_w, wmin, wmax)
+            if self.curriter % 10 == 0: print "it={}  data_loss={:.2E}  reg_loss={:.2E}  lr={:.2E}  orig_grad={:.2E}  clip_grad={:.2E}  {}  stepsize={:.2E}  wnorm={:.2E}  wmin,max={:.2E},{:.2E}".format(self.curriter, data_loss, reg_loss, self.lr, np.linalg.norm(grad), np.linalg.norm(grad), ("C" if False else "."), norm_wdelta, norm_w, wmin, wmax)
 
             # Update w in place
             w[...] += wdelta